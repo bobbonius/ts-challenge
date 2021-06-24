ca4762e14c33267df05ffe67ae7399a8cb54269b
bfmuller <bob.muller@capgemini.com>
2021-06-24 19:31:30 +0200

    add input component

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,28 @@
+import styled from '@emotion/styled'
+import { Input, space, sizes } from '@ticketswap/solar'
+import { MagnifyingGlass } from '@ticketswap/comets'
+
+const SearchContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+  padding: ${space[16]};
+  margin: ${space[16]} auto;
+  max-width: ${sizes.tablet}px;
+`
+
 export default function Search() {
   return (
     <>
-      <p>Search page</p>
+      <SearchContainer>
+      <Input
+            placeholder="Search for events"
+            id="search"
+            label="Search"
+            hideLabel
+            leftAdornment={<MagnifyingGlass size={24} />}
+          />
+      </SearchContainer>
     </>
   )
 }
